6bdebdfb21c27755b63bed361602fb19a5c2839e
Ariful Islam <arifulislam161024@gmail.com>
2022-01-10 10:26:54 +0600

    conditional mapDispatch & mapState

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import CakeContainer from "./components/CakeContainer";
 import HookIceCreamContainer from "./components/HookIceCreamContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
+import ItemContainer from "./components/ItemContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
 import store from "./redux/store";
 function App() {
   return (
     <Provider store={store}>
       <div className='App'>
+        <ItemContainer cake />
+        <ItemContainer />
         <CakeContainer />
         <HooksCakeContainer />
         <IceCreamContainer />
